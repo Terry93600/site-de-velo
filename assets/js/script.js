1cb706f5a46bff7daa71638491a8f96edc87c690
Terry93600 <terrygenly.tg@gmail.com>
2022-08-11 10:32:56 +0200

    ajout

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,34 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }))
 
-let choix = true;
+// let choix = true;
 
-function change () {
-    let button = document.getElementById("button");
-    if (choix == true){
-        button.style.backgroundColor = "green";
-        choix = false;
-    }
-    else {
-        button.style.backgroundColor = "pink";
-        choix = true;
-    }
-    
-}
+// function change () {
+//     let button = document.getElementById("button");
+//     if (choix == true){
+//         button.style.backgroundColor = "green";
+//         choix = false;
+//     }
+//     else {
+//         button.style.backgroundColor = "pink";
+//         choix = true;
+//     }  
+// }  
+
+let colors = ["bleu", "yellow", "black", "pink", "violet"];
+let police = ["cursive"]
+// let button = document.getElementById("button");
+let button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+    var randomColor = colors [Math.floor(Math.random() * colors.length)]
+    // let container = document.getElementById("container");
+    let header = document.getElementById("header");
+
+    // container.style.background = randomColor;
+    header.style.background = randomColor;
+
+    let typo = document.getElementById("typo");
+
+    typo.style.fontfamilly = police;
+})
